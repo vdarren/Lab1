30d0841e2347db963a1b45aad09d2990686962f1
unknown <1303043@RG-N533-F06.rgu.ac.uk>
2016-10-03 14:48:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1303043
+ * Date: 03/10/2016
+ * Time: 14:08
+ */
+
+<!DOCTYPE html >
+
+    <html >
+
+        <head >
+
+        </head >
+
+        <body >
+
+        <p >
+
+            <?php
+
+            echo "Hello World";
+            echo "Hello," . "world" . "!";
+            echo 5+8;
+
+            $myname = "Darren Williamson";
+            $myage = 21;
+            echo "My name is " . $myname . " and I am " . $myage;
+
+            $name = "Ryan";
+            if ($name == "Simon"){
+                print "I know you";
+            }
+            else{
+                print "Who are you";
+            }
+            
+            $myage1 = 0;
+            if ($my)
+
+
+            ?>
+
+        </p>
+
+    </body>
+
+</html>
